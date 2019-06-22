776ca3cd5456589890d1450d4c90401407792652
Daniel Weisberg <DWEISBER@uwaterloo.ca>
2019-06-22 15:50:18 -0400

    Allowed for application to open on NFC

--- Tapp/app/src/main/java/com/goose/tapp/MainActivity.java
+++ Tapp/app/src/main/java/com/goose/tapp/MainActivity.java
@@ -1,13 +1,45 @@
 package com.goose.tapp;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.title);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText("Everything works with NFC!");
+        }
+
+        handleIntent(getIntent());
     }
+
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
+    }
+
 }
