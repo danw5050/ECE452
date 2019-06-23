83e0acce3154cb42d8864ed5595e040db526b28e
Gunin Khanna <gunin@ymail.com>
2019-06-23 12:55:08 -0400

    added TAPP header

--- Tapp/app/src/main/java/com/goose/tapp/NFCListActivity.java
+++ Tapp/app/src/main/java/com/goose/tapp/NFCListActivity.java
@@ -9,5 +9,7 @@ public class NFCListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_list);
+        getSupportActionBar().hide();
+
     }
 }
--- Tapp/app/src/main/java/com/goose/tapp/NFCTriggerActivity.java
+++ Tapp/app/src/main/java/com/goose/tapp/NFCTriggerActivity.java
@@ -13,6 +13,7 @@ public class NFCTriggerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_trigger);
+
     }
 
     @Override
