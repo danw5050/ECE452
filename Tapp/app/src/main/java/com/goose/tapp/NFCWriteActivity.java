255036ccb1d5710d11890ab4a2171726e2f1d8d6
Daniel Weisberg <DWEISBER@uwaterloo.ca>
2019-07-06 17:53:11 -0400

    Added writing ability

--- Tapp/app/src/main/java/com/goose/tapp/NFCWriteActivity.java
+++ Tapp/app/src/main/java/com/goose/tapp/NFCWriteActivity.java
@@ -1,6 +1,102 @@
 package com.goose.tapp;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Locale;
 
 public class NFCWriteActivity extends AppCompatActivity {
+
+    TextView writingStatus;
+    NFCManager nfcMger;
+    Tag currentTag;
+    private NdefMessage message = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_write);
+        writingStatus = findViewById(R.id.writingStatus);
+
+        nfcMger = new NFCManager(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        message =  nfcMger.createTextMessage("my test!");
+
+        Intent nfcIntent = new Intent(this, getClass());
+        nfcIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
+        IntentFilter[] intentFiltersArray = new IntentFilter[] {};
+        String[][] techList = new String[][] {
+                { android.nfc.tech.Ndef.class.getName() },
+                { android.nfc.tech.NdefFormatable.class.getName() }
+        };
+        NfcAdapter nfcAdpt = NfcAdapter.getDefaultAdapter(this);
+        nfcAdpt.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techList);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // It is the time to write the tag
+        currentTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if (message != null) {
+            writingStatus.setText("NFC Tripped");
+            nfcMger.writeTag(currentTag, message);
+
+        }
+        else {
+            // Handle intent
+
+        }
+    }
+
+
+    public void writeTag(Tag tag, NdefMessage message)  {
+        if (tag != null) {
+            try {
+                Ndef ndefTag = Ndef.get(tag);
+                if (ndefTag == null) {
+                    // Let's try to format the Tag in NDEF
+                    NdefFormatable nForm = NdefFormatable.get(tag);
+                    if (nForm != null) {
+                        nForm.connect();
+                        nForm.format(message);
+                        nForm.close();
+                    }
+                }
+                else {
+                    ndefTag.connect();
+                    ndefTag.writeNdefMessage(message);
+                    ndefTag.close();
+                }
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
