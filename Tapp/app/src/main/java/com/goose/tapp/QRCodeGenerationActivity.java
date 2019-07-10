3f09035f123a69c8860b2611897986f9b20f0f01
Daniel Weisberg <DWEISBER@uwaterloo.ca>
2019-07-10 18:28:56 -0400

    WIP

---
+++ Tapp/app/src/main/java/com/goose/tapp/QRCodeGenerationActivity.java
@@ -0,0 +1,13 @@
+package com.goose.tapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class QRCodeGenerationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code_generator);
+    }
+}
