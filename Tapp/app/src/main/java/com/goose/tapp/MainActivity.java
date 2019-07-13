a854d8bcb84dfce632fa6fa11dd8b18caf0529af
Daniel Weisberg <DWEISBER@uwaterloo.ca>
2019-07-13 19:48:25 -0400

    Added temp example of recycler list population from firebase

--- Tapp/app/src/main/java/com/goose/tapp/MainActivity.java
+++ Tapp/app/src/main/java/com/goose/tapp/MainActivity.java
@@ -4,24 +4,54 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.auth.FirebaseUser;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private NfcAdapter mNfcAdapter;
+
+    // Required global layouts
     private RelativeLayout mainActivityRootView;
+    private RecyclerView nfcListListView;
+
+
+
+
+    DatabaseReference databaseReference;
+    List<StudentDetails> list = new ArrayList<>();
+    RecyclerView.Adapter adapter ;
+
+
+    // Firebase Authentication
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authListener;
 
+    // Reference to current user for entire application
+    public static FirebaseUser user;
 
 
     @Override
@@ -31,13 +61,16 @@ public class MainActivity extends AppCompatActivity {
         // Setup the activity views
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        Button studyTable = findViewById(R.id.studyTable);
+        nfcListListView = findViewById(R.id.nfcListListView);
         Button logout = findViewById(R.id.logout);
         mainActivityRootView = findViewById(R.id.mainActivityRootView);
 
-        // Ensure user is logged in
-        auth = FirebaseAuth.getInstance();
 
+        // Get the user currently logged in
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        // Ensure user is logged in
         authListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -62,21 +95,74 @@ public class MainActivity extends AppCompatActivity {
             nfcError(getApplicationContext().getString(R.string.nfc_disabled));
         }
 
-
-        // Setup the activity listeners
-        studyTable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                studyTable();
-            }
-        });
-
+        // Setup on click listeners
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Sign the user out
                 auth.signOut();
             }
         });
+
+
+
+        //Setup firebase
+        //nfcListListView.setHasFixedSize(true);
+
+        nfcListListView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("student");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                    StudentDetails studentDetails = dataSnapshot.getValue(StudentDetails.class);
+
+                    list.add(studentDetails);
+                    Log.d("nfcRecyclerView", studentDetails.getName());
+
+
+
+
+
+
+
+                }
+
+                adapter.notifyDataSetChanged();
+               // progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+               // progressDialog.dismiss();
+
+            }
+        });
+
+        adapter = new RecyclerViewAdapter(MainActivity.this, list);
+        nfcListListView.setAdapter(adapter);
+
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
     }
 
     /*
@@ -102,17 +188,4 @@ public class MainActivity extends AppCompatActivity {
         snackbar.show();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        auth.addAuthStateListener(authListener);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (authListener != null) {
-            auth.removeAuthStateListener(authListener);
-        }
-    }
 }
---
+++ Tapp/app/src/main/java/com/goose/tapp/RecyclerViewAdapter.java
@@ -0,0 +1,65 @@
+package com.goose.tapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    Context context;
+    List<StudentDetails> MainImageUploadInfoList;
+
+    public RecyclerViewAdapter(Context context, List<StudentDetails> TempList) {
+
+        this.MainImageUploadInfoList = TempList;
+
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_items, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        StudentDetails studentDetails = MainImageUploadInfoList.get(position);
+
+        holder.StudentNameTextView.setText(studentDetails.getName());
+
+        holder.StudentNumberTextView.setText(studentDetails.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return MainImageUploadInfoList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView StudentNameTextView;
+        public TextView StudentNumberTextView;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+
+            StudentNameTextView = (TextView) itemView.findViewById(R.id.ShowStudentNameTextView);
+
+            StudentNumberTextView = (TextView) itemView.findViewById(R.id.ShowStudentNumberTextView);
+        }
+    }
+}
